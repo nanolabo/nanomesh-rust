e12526b886dedd26aae075054d5d0b6f6aae850a
Olivier Giniaux <oginiaux@gmail.com>
2022-04-23 23:09:44 +0200

    Handle download of files

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,10 +5,10 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn read_obj(bytes: &[u8]) -> i32 {
+pub fn read_obj(bytes: &[u8]) -> Vec<u8> {
   let mut sum = 0i32;
   for i in 0..bytes.len() {
     sum += bytes[i] as i32;
   }
-  return sum;
+  return Vec::new();
 }
