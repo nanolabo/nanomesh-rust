ef7e1301c393597e67ee7646688ef1dcb1563d05
Olivier Giniaux <oginiaux@gmail.com>
2022-04-24 22:21:14 +0200

    Add support for processing multiple files at once

--- wasm/callbacks.js
+++ wasm/callbacks.js
@@ -1,3 +1,3 @@
 export function set_progress(progress, status) {
-  console.log(status + "[" + 100 * progress + "%]");
+  console.log("[" + 100 * progress + "%] " + status);
 }
---
+++ wasm/nanolabo.js
@@ -0,0 +1,89 @@
+import init, { read_obj } from "./pkg/nanolabo_wasm.js";
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    // Create file reader
+    let reader = new FileReader();
+
+    // Register event listeners
+    reader.addEventListener("loadend", (e) => resolve(e.target.result));
+    reader.addEventListener("error", reject);
+
+    // Read file
+    reader.readAsArrayBuffer(file);
+  });
+}
+
+// https://localcoder.org/passing-client-files-to-webassembly-from-the-front-end
+async function handleFile(file) {
+  // Todo: Stop copying in a JS buffer, instead copy direct to a wasm buffer
+  // https://wasmbyexample.dev/examples/webassembly-linear-memory/webassembly-linear-memory.rust.en-us.html#
+  var array = new Uint8Array(await readFile(file));
+  console.log(array);
+  var result = read_obj(array);
+
+  // Download result back
+  var blob = new Blob([result], { type: "application/pdf" }); // change resultByte to bytes
+  var link = document.createElement("a");
+  link.href = window.URL.createObjectURL(blob);
+  link.download = "output.obj";
+  link.click();
+}
+
+// Initialize wasm module
+init();
+
+// Selecting all required elements
+const dropArea = document.getElementById("drag-area");
+const dragText = dropArea.querySelector("header");
+const button = dropArea.querySelector("button");
+const input = dropArea.querySelector("input");
+
+button.onclick = () => {
+  input.click(); // If user click on the button then the input also clicked
+};
+
+input.onchange = function () {
+  // Getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  dropArea.classList.add("active");
+  showFile(this.files);
+};
+
+// If user Drag File Over DropArea
+dropArea.ondragover = function (event) {
+  event.preventDefault(); // Preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+};
+
+// If user leave dragged File from DropArea
+dropArea.ondragleave = function () {
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+};
+
+//If user drop File on DropArea
+dropArea.ondrop = function (event) {
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  showFile(event.dataTransfer.files); //calling function
+};
+
+function showFile(files) {
+  let validExtensions = ["obj"]; // Adding some valid image extensions in array
+
+  for (var i = 0; i < files.length; i++) {
+    console.log(files[i]);
+    let format = files[i].name.split(".").pop();
+    if (!validExtensions.includes(format)) {
+      alert("This is not a supported file format!");
+      dropArea.classList.remove("active");
+      dragText.textContent = "Drag & Drop to Upload File";
+      return;
+    }
+  }
+
+  for (var i = 0; i < files.length; i++) {
+    handleFile(files[i]);
+  }
+}
