2617546c103fd74cf5f5f9e053cca80d0946237c
Olivier Giniaux <oginiaux@gmail.com>
2022-04-24 15:07:24 +0200

    Implement progress callbacks

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -4,11 +4,18 @@
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen(module = "/callbacks.js")]
+extern {
+    fn set_progress(progress: f32, status: &str);
+}
+
 #[wasm_bindgen]
 pub fn read_obj(bytes: &[u8]) -> Vec<u8> {
+  set_progress(0., "Importing...");
   let mut sum = 0i32;
   for i in 0..bytes.len() {
     sum += bytes[i] as i32;
   }
+  set_progress(1., "Done!");
   return Vec::new();
 }
--- wasm/www/nanolabo_wasm_bg.wasm.d.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function read_obj(a: number, b: number): number;
-export function __wbindgen_malloc(a: number): number;
