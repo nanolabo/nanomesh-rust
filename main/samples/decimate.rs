5e5ab23d812d9cbbcfc8fdcf6b911f521019e041
Olivier Giniaux <oginiaux@smartadserver.com>
2024-08-11 09:44:46 +0200

    Commit?

--- main/samples/decimate.rs
+++ main/samples/decimate.rs
@@ -4,12 +4,18 @@
 #[path = "../src/lib.rs"]
 mod nanomesh;
 
-use std::time::Instant;
+use std::{time::Instant, fs::File, io::{BufReader, BufWriter}};
 
 fn main()
 {
+    let input = File::open("main/samples/sphere_flat_hp.obj").unwrap();
+    let mut reader: BufReader<File> = BufReader::new(input);
+
+    let output = File::create("main/samples/output.obj").unwrap();
+    let mut writer = BufWriter::new(output);
+
     let now = Instant::now();
-    let shared_mesh = nanomesh::io::obj::read("./sphere_flat_hp.obj");
+    let shared_mesh = nanomesh::io::obj::read(&mut reader);
     println!("read obj done in {} ms", now.elapsed().as_millis());
 
     let now = Instant::now();
@@ -17,7 +23,7 @@ fn main()
     println!("to connected mesh done in {} ms", now.elapsed().as_millis());
 
     let now = Instant::now();
-    mesh.decimate_to_ratio(0.5);
+    mesh.decimate_to_ratio(0.99);
     println!("decimation done in {} ms", now.elapsed().as_millis());
 
     let now = Instant::now();
@@ -25,6 +31,6 @@ fn main()
     println!("to shared mesh done in {} ms", now.elapsed().as_millis());
 
     let now = Instant::now();
-    nanomesh::io::obj::write(shared_mesh, "./output.obj");
+    nanomesh::io::obj::write(&shared_mesh, &mut writer);
     println!("write obj done in {} ms", now.elapsed().as_millis());
 }
